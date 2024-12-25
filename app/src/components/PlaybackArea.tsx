cc82e1f755dc3ba6c7cd572f6eb9318c3528c945
Jon Zudell <jonathon.zudell@gmail.com>
2024-12-24 22:25:09 -0600

    two videos and swap between for minimal reset latency

--- app/src/components/PlaybackArea.tsx
+++ app/src/components/PlaybackArea.tsx
@@ -30,26 +30,29 @@ const PlaybackArea: React.FC<PlaybackAreaProps> = ({ leftMenuWidth }) => {
   const loopEnd = useSelector(selectLoopEnd);
   const looping = useSelector(selectLooping);
 
-  const videoRef = useRef<HTMLVideoElement | null>(null);
+  const videoRef1 = useRef<HTMLVideoElement | null>(null);
+  const videoRef2 = useRef<HTMLVideoElement | null>(null);
   const containerRef = useRef<HTMLDivElement | null>(null);
   const isDraggingRef = useRef(false);
   const isPlayingBeforeDragRef = useRef(false);
   const [videoWidth, setVideoWidth] = useState(0);
+  const [activeVideo, setActiveVideo] = useState(1);
 
   useEffect(() => {
-    if (videoRef.current) {
+    if (videoRef1.current && videoRef2.current) {
+      const activeVideoRef = activeVideo === 1 ? videoRef1.current : videoRef2.current;
       if (playing) {
-        videoRef.current.play();
+        activeVideoRef.play();
       } else {
-        videoRef.current.pause();
+        activeVideoRef.pause();
       }
     }
-  }, [playing]);
+  }, [playing, activeVideo]);
 
   useEffect(() => {
     const handleResize = () => {
-      if (videoRef.current) {
-        setVideoWidth(videoRef.current.clientWidth);
+      if (videoRef1.current) {
+        setVideoWidth(videoRef1.current.clientWidth);
       }
     };
 
@@ -61,56 +64,61 @@ const PlaybackArea: React.FC<PlaybackAreaProps> = ({ leftMenuWidth }) => {
   }, []);
 
   useEffect(() => {
-    if (videoRef.current) {
-      setVideoWidth(videoRef.current.clientWidth);
+    if (videoRef1.current) {
+      setVideoWidth(videoRef1.current.clientWidth);
     }
-  }, [media, videoRef.current?.clientWidth, leftMenuWidth]);
+  }, [media, videoRef1.current?.clientWidth, leftMenuWidth]);
 
   useEffect(() => {
     const interval = setInterval(() => {
-      if (playing && videoRef.current) {
+      const activeVideoRef = activeVideo === 1 ? videoRef1.current : videoRef2.current;
+      const inactiveVideoRef = activeVideo === 1 ? videoRef2.current : videoRef1.current;
+
+      if (playing && activeVideoRef) {
         if (
           media &&
-          videoRef.current.currentTime >= loopEnd * media.duration &&
+          activeVideoRef.currentTime >= loopEnd * media.duration &&
           looping
         ) {
           const newTimeElapsed = loopStart * media.duration;
-          videoRef.current.currentTime = newTimeElapsed;
-          videoRef.current.play();
+          inactiveVideoRef.play();
+          activeVideoRef.pause();
+          activeVideoRef.currentTime = newTimeElapsed;
+          setActiveVideo(activeVideo === 1 ? 2 : 1);
           dispatch(setTimeElapsed(newTimeElapsed));
         } else if (
           media &&
-          videoRef.current.currentTime < loopStart * media.duration &&
+          activeVideoRef.currentTime < loopStart * media.duration &&
           looping
         ) {
           const newTimeElapsed = loopStart * media.duration;
-          videoRef.current.currentTime = newTimeElapsed;
-          videoRef.current.play();
+          activeVideoRef.currentTime = newTimeElapsed;
+          activeVideoRef.play();
           dispatch(setTimeElapsed(newTimeElapsed));
-        } else if (media && videoRef.current.currentTime >= media.duration) {
+        } else if (media && activeVideoRef.currentTime >= media.duration) {
           const newTimeElapsed = media.duration;
-          videoRef.current.currentTime = newTimeElapsed;
-          videoRef.current.pause();
+          activeVideoRef.currentTime = newTimeElapsed;
+          activeVideoRef.pause();
         } else {
-          dispatch(setTimeElapsed(videoRef.current.currentTime));
+          dispatch(setTimeElapsed(activeVideoRef.currentTime));
         }
-      } else if (!playing && videoRef.current) {
-        videoRef.current.currentTime = timeElapsed;
+      } else if (!playing && activeVideoRef) {
+        activeVideoRef.currentTime = timeElapsed;
       }
     }, 50);
 
     return () => clearInterval(interval);
-  }, [playing, dispatch, timeElapsed, media, loopEnd, looping, loopStart]);
+  }, [playing, dispatch, timeElapsed, media, loopEnd, looping, loopStart, activeVideo]);
 
   useEffect(() => {
-    if (media && videoRef.current) {
+    if (media && videoRef1.current) {
       dispatch(setMedia(media)); // Dispatch the media object without videoRef
     }
-  }, [media, videoRef, dispatch]);
+  }, [media, videoRef1, dispatch]);
 
   const handleLoadedMetadata = () => {
-    if (videoRef.current) {
-      videoRef.current.currentTime = timeElapsed;
+    if (videoRef1.current) {
+      videoRef1.current.currentTime = timeElapsed;
       if (media) {
         dispatch(setMedia(media)); // Dispatch the media object without videoRef
       }
@@ -126,16 +134,28 @@ const PlaybackArea: React.FC<PlaybackAreaProps> = ({ leftMenuWidth }) => {
             style={{ zIndex: -1 }}
           >
             {media && (
-              <video
-                ref={videoRef}
-                controls={false}
-                className="responsive-video"
-                loop={looping}
-                onLoadedMetadata={handleLoadedMetadata}
-              >
-                <source src={media.url} type={media.fileType} />
-                Your browser does not support the video tag.
-              </video>
+              <>
+                <video
+                  ref={videoRef1}
+                  controls={false}
+                  className={`responsive-video ${activeVideo === 1 ? 'visible' : 'hidden'}`}
+                  loop={false} // Disable native looping
+                  onLoadedMetadata={handleLoadedMetadata}
+                >
+                  <source src={media.url} type={media.fileType} />
+                  Your browser does not support the video tag.
+                </video>
+                <video
+                  ref={videoRef2}
+                  controls={false}
+                  className={`responsive-video ${activeVideo === 2 ? 'visible' : 'hidden'}`}
+                  loop={false} // Disable native looping
+                  onLoadedMetadata={handleLoadedMetadata}
+                >
+                  <source src={media.url} type={media.fileType} />
+                  Your browser does not support the video tag.
+                </video>
+              </>
             )}
           </div>
           {media && (
@@ -156,7 +176,7 @@ const PlaybackArea: React.FC<PlaybackAreaProps> = ({ leftMenuWidth }) => {
                 timeElapsed={timeElapsed}
                 isDraggingRef={isDraggingRef}
                 isPlayingBeforeDragRef={isPlayingBeforeDragRef}
-                videoRef={videoRef}
+                videoRef={activeVideo === 1 ? videoRef1 : videoRef2}
               />
             </div>
           )}
