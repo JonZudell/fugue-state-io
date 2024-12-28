85fa5e6c25340421ae42400dae36ea2ab7896ef3
Jon Zudell <jonathon.zudell@gmail.com>
2024-12-28 17:01:26 -0600

    fix next

---
+++ app/src/components/WaveformVisualizer.tsx
@@ -0,0 +1,115 @@
+"use client";
+import { useEffect, useRef } from "react";
+import { Summary } from "../core/waveformSummary";
+import { selectTimeElapsed } from "@/store/playbackSlice";
+import { useSelector } from "react-redux";
+import { FileState } from "@/store/filesSlice";
+import { getSlice } from "../core/waveformSummary";
+interface WaveformVisualizerProps {
+  media?: FileState;
+  width?: number;
+  summary?: Summary;
+  channel?: string;
+  startPercentage?: number;
+  endPercentage?: number;
+  height?: number;
+}
+
+const WaveformVisualizer: React.FC<WaveformVisualizerProps> = ({
+  media,
+  channel = "L",
+  startPercentage = 0,
+  endPercentage = 100,
+  width = 1000,
+  height = 200,
+}) => {
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+  const timeElapsed = useSelector(selectTimeElapsed);
+
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    if (!canvas) {
+      return;
+    }
+    const ctx = canvas.getContext("2d");
+    if (!ctx) {
+      return;
+    }
+
+    // Set canvas width to container width
+    const container = canvas.parentElement;
+    if (container) {
+      canvas.width = container.clientWidth;
+      canvas.height = container.clientHeight / 2; // Set height to half of container
+    }
+
+    // clear canvas
+    ctx.fillStyle = "white";
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    const drawWaveform = () => {
+      console.log("drawing waveform", width, height);
+      if (media && media.summary && canvas) {
+        if (!ctx) {
+          return;
+        }
+        if (channel === "L") {
+          ctx.fillStyle = "white";
+          const channelSummary = media.summary.channels.left?.count || 0;
+          const startCount = Math.floor(
+            (channelSummary * startPercentage) / 100,
+          );
+          const endCount = Math.floor((channelSummary * endPercentage) / 100);
+          const samples = endCount - startCount;
+          const samplesPerPixel = samples / canvas.width;
+          const data = media.summary.channels.left;
+          if (!data) {
+            return;
+          }
+          for (let i = 0; i < canvas.width; i++) {
+            const start = startCount + i * samplesPerPixel;
+            const end = start + samplesPerPixel;
+            const slice = getSlice(data, start, end);
+            const yMin = ((1 - slice.low) * canvas.height) / 2;
+            const yMax = ((1 - slice.high) * canvas.height) / 2;
+            ctx.fillRect(i, yMin, 1, yMax - yMin);
+          }
+        } else if (channel === "R") {
+          ctx.fillStyle = "white";
+          const channelSummary = media.summary.channels.right?.count || 0;
+          const startCount = Math.floor(
+            (channelSummary * startPercentage) / 100,
+          );
+          const endCount = Math.floor((channelSummary * endPercentage) / 100);
+          const samples = endCount - startCount;
+          const samplesPerPixel = samples / canvas.width;
+          const data = media.summary.channels.right;
+          if (!data) {
+            return;
+          }
+          for (let i = 0; i < canvas.width; i++) {
+            const start = startCount + i * samplesPerPixel;
+            const end = start + samplesPerPixel;
+            const slice = getSlice(data, start, end);
+            const yMin = ((1 - slice.low) * canvas.height) / 2;
+            const yMax = ((1 - slice.high) * canvas.height) / 2;
+            ctx.fillRect(i, yMin, 1, yMax - yMin);
+          }
+        }
+      }
+    };
+
+    drawWaveform();
+  }, [timeElapsed, channel, startPercentage, endPercentage, media, width]);
+
+  return (
+    <canvas
+      ref={canvasRef}
+      width={width}
+      height={height}
+      className="w-full"
+      style={{ width: "100%", height: "50%" }}
+    ></canvas>
+  );
+};
+export default WaveformVisualizer;
